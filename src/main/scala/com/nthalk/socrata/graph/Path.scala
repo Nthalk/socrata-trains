7207a9f54a532da68e70ebde249fb0ff424f8321
Carl Taylor <carl@etaylor.me>
2014-07-17 01:00:32 -0700

    Removed unused methods

--- src/main/scala/com/nthalk/socrata/graph/Path.scala
+++ src/main/scala/com/nthalk/socrata/graph/Path.scala
@@ -1,13 +1,3 @@
 package com.nthalk.socrata.graph
 
-case class Path(graph: Graph, edges: List[Edge[_, _]]) {
-  def outP(): Iterator[Path] = {
-    edges.last.to.outE.map { c => copy(edges = edges :+ c) }
-  }
-
-  override def toString(): String = {
-    val s = edges.foldLeft("") { (s, e) => s"${s},${e.toString}" }
-    s"P(${s.substring(1)})"
-  }
-}
-
+case class Path(graph: Graph, edges: List[Edge[_, _]])
--- src/main/scala/com/nthalk/socrata/graph/Vertex.scala
+++ src/main/scala/com/nthalk/socrata/graph/Vertex.scala
@@ -6,13 +6,6 @@ import scala.collection.mutable.Map
  * Defines a vertex with an identifying object of obj
  */
 case class Vertex[T](graph: Graph, val obj: T) {
-  def outE(): Iterator[Edge[_, _]] = {
-    graph.outE(this)
-  }
-
-  def outPath(): Iterator[Path] = {
-    outE.map { o => Path(graph, List(o)) }
-  }
 
   def data(): Map[String, Any] = {
     graph.data(this)
